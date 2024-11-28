6e9d190d048d0440cbee8489347fd0f977427b9b
cristian-agualimpia <cagualimpia@unal.edu.co>
2024-11-28 08:11:35 -0500

    first commit

---
+++ src/main/java/com/proyecto/uncampus/back_uncampus/BackUncampusApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.uncampus.back_uncampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackUncampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackUncampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/uncampus/back_uncampus/BackUncampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.uncampus.back_uncampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackUncampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
